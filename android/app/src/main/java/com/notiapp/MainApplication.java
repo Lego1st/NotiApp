beadb7ef8b5169fe634972be808787184da4f694
Nghia <nguyentrungnghia1812@gmail.com>
2017-01-14 14:22:20 +0700

    commit 3

--- android/app/src/main/java/com/notiapp/MainApplication.java
+++ android/app/src/main/java/com/notiapp/MainApplication.java
@@ -13,7 +13,7 @@ import com.facebook.soloader.SoLoader;
 import java.util.Arrays;
 import java.util.List;
 
-import com.geektime.reactnativeonesignal.ReactNativeOneSignalPackage;
+import com.geektime.reactnativeonesignal.ReactNativeOneSignalPackage; 
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -26,7 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
           new ReactNativeOneSignalPackage()
       );
     }
